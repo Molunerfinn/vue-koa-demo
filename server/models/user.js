366ea43c15620a7f6065d78de3078f4312c9c7a0
Molunerfinn <marksz@teamsz.xyz>
2017-05-01 22:33:22 +0800

    Fixed: #4

--- server/models/user.js
+++ server/models/user.js
@@ -4,8 +4,8 @@ const TodolistDb = db.Todolist; // 引入数据库
 
 const User = TodolistDb.import(userModel); // 用sequelize的import方法引入表结构，实例化了User。
 
-const getUserById = async function(id){ // 注意是function* 而不是function 对于需要yield操作的函数都需要这种generator函数。
-  const userInfo = await User.findOne({ // 用yield控制异步操作，将返回的Promise对象里的数据返回出来。也就实现了“同步”的写法获取异步IO操作的数据
+const getUserById = async function(id){ // 注意是async function 而不是function。对于需要等待promise结果的函数都需要async await。
+  const userInfo = await User.findOne({ // 用await控制异步操作，将返回的Promise对象里的数据返回出来。也就实现了“同步”的写法获取异步IO操作的数据
     where: {
       id: id
     }
