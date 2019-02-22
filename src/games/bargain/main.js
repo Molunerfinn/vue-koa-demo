04248dbbe6a67f9129e585792eb0c5885625916f
qinghe <david@getstore.cn>
2019-02-22 09:41:37 +0800

    add module weui, jweixin

--- src/games/bargain/main.js
+++ src/games/bargain/main.js
@@ -6,9 +6,15 @@ import App from './App'
 // import 'element-ui/lib/theme-default/index.css'
 // import VueRouter from 'vue-router'
 import axios from 'axios'
+import wx from 'weixin-js-sdk'
+import weui from 'weui'
+
+import '@/assets/bargain/css/common-wx.css'
+import '@/assets/bargain/css/game-wx.css'
 
 Vue.prototype.$http = axios // 类似于vue-resource的调用方法
-
+Vue.prototype.$wx = wx
+Vue.prototype.$weui = weui
 /* eslint-disable no-new */
 new Vue({
   render: h => h(App)
