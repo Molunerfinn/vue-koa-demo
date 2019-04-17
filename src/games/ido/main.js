bfd65a0206a7faf5073e9f9db7558eafef1f98b4
zhengxiuyu-at-getstore <zhengxiuyu@getstore.cn>
2019-04-17 10:50:33 +0800

    add swiper remove el-ui

--- src/games/ido/main.js
+++ src/games/ido/main.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false
 
