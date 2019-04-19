88a451ff87609ba165e9075af17eaa7dcc3a2a6d
zhengxiuyu-at-getstore <zhengxiuyu@getstore.cn>
2019-04-19 09:08:36 +0800

    fix bug

--- server/controllers/api/game/bargain.js
+++ server/controllers/api/game/bargain.js
@@ -7,25 +7,9 @@
 
 'use strict';
 import fetch from 'node-fetch'
-<<<<<<< HEAD:server/controllers/api/games/bargain.js
-import {
-  Sequelize,
-  GameRoundBargain,
-  GamePlayer,
-  GameResult,
-  GameDay
-} from '../../../models'
-import {
-  GameRoundStates
-} from '../../../schema/constant'
-import {
-  FailMessage
-} from '../../constant'
-=======
 import { Sequelize, BargainGameRound,  GamePlayer, GameResult, GameDay } from '../../../models'
 import { GameRoundStates } from '../../../schema/constant'
 import { FailMessage } from '../../constant'
->>>>>>> c31f2055345e8e77236cafbb1e884c2f395c108d:server/controllers/api/game/bargain.js
 import log4 from 'koa-log4'
 const logger = log4.getLogger('index')
 
@@ -55,27 +39,6 @@ class Bargain {
         id: game_player_id,
         game_round_id
       }
-<<<<<<< HEAD:server/controllers/api/games/bargain.js
-    })
-    let to_game_player = game_player
-    if (to_game_player_id) {
-      to_game_player = (await GamePlayer.findOne({
-        where: {
-          id: to_game_player_id,
-          game_round_id
-        }
-      })) || game_player
-    }
-    if (game_player) {
-
-      let game_round = await GameRoundBargain.findById(game_round_id);
-      // 检查当前用户的game_day
-      let game_day = GameDay.findOrCreate({
-        where: {
-          game_round_id,
-          game_player_id,
-          day: new Date()
-=======
       if( game_player ){
 
         let game_round = await BargainGameRound.findById(game_round_id);
@@ -118,7 +81,6 @@ class Bargain {
         let url = 'play-wx'
         if( to_game_player_id ){
           url= `${url}?to_game_player_id=${to_game_player_id}`
->>>>>>> c31f2055345e8e77236cafbb1e884c2f395c108d:server/controllers/api/game/bargain.js
         }
       })
 
@@ -140,27 +102,6 @@ class Bargain {
       };
       await ctx.render('games/bargain/error-wx', context);
     }
-<<<<<<< HEAD:server/controllers/api/games/bargain.js
-    console.log(1, "playWx")
-
-  }
-
-  /**
-   * 微信用户注册
-   * GET /game-yiy/14/checkin-wx?openid=oF9hV0SyZ6tI_k2WHtpRXqfedRH4
-   * parmas game_round_id, headimgurl, nickname, openid
-   */
-  static async checkinWx(ctx) {
-
-    let game_round_id = ctx.params.id
-    let openid = ctx.query.openid
-    let to_game_player_id = ctx.query.to_game_player_id //被助力人id
-    const current_game = await GameRoundBargain.findById(game_round_id);
-    const current_player = await GamePlayer.findOne({
-      where: {
-        game_round_id,
-        openid
-=======
 
     // 取得助力游戏基本信息
     static async gameInfoWx( ctx ){
@@ -190,7 +131,6 @@ console.log( "before to_game_player_id")
       }else{
         // 砍价日志的获取判断是否砍过
         game_result = await GameResult.findOne({ where:{ game_player_id, game_round_id, to_game_player_id: game_player_id }})
->>>>>>> c31f2055345e8e77236cafbb1e884c2f395c108d:server/controllers/api/game/bargain.js
       }
     });
     const context = {
@@ -228,102 +168,6 @@ console.log( "before to_game_player_id")
       ctx.redirect(url);
     }
 
-<<<<<<< HEAD:server/controllers/api/games/bargain.js
-  }
-
-  // 取得助力游戏基本信息
-  static async gameInfoWx(ctx) {
-    console.log(0, "gameInfoWx")
-
-    let game_round_id = ctx.query.game_round_id
-    let game_player_id = ctx.query.game_player_id //助力人id
-    let to_game_player_id = ctx.query.to_game_player_id //被助力人id，可能为空
-    let url = ctx.query.url // 可能有 to_game_player_id 或者没有
-
-    const game_round = await GameRoundBargain.findById(game_round_id);
-
-    const game_player = await GamePlayer.findById(game_player_id);
-
-    // 参加此活动总人数和排名
-    let game_player_rank = await GamePlayer.findAll({
-      where: {
-        game_round_id
-      },
-      order: [
-        ['score', 'DESC'],
-        ['id', 'ASC']
-      ],
-      limit: 20,
-      offset: 0,
-    })
-    //$bargain_user_all = $user_mod->where(array("bargain_id" => $bargain_id))->order('new_price asc,id DESC')->limit("0,20")->select();
-    let game_result = null;
-    let to_game_player = game_player; // 被助力人缺省情况是自己
-    console.log("before to_game_player_id")
-    if (to_game_player_id) { // 如果有被助力人
-      to_game_player = await GamePlayer.findById(to_game_player_id);
-      // 判断是否为本机的活动 我要参与
-      // 砍价日志的获取判断是否砍过
-      game_result = await GameResult.findOne({
-        where: {
-          game_player_id,
-          game_round_id,
-          to_game_player_id: to_game_player_id
-        }
-      })
-      //$list_log = M("bargain_log")->where(array("user_id" => $user_id, "openid" => $openid, "bargain_id" => $bargain_id))->select();
-    } else {
-      // 砍价日志的获取判断是否砍过
-      game_result = await GameResult.findOne({
-        where: {
-          game_player_id,
-          game_round_id,
-          to_game_player_id: game_player_id
-        }
-      })
-    }
-    // 有多少助力成绩
-    let game_result_rank = await GameResult.findAll({
-      where: {
-        game_round_id,
-        to_game_player_id: to_game_player.id
-      },
-      include: [{
-        model: GamePlayer
-      }],
-      limit: 20,
-      offset: 0
-    })
-    //let giving_GamePlayer = await GamePlayer.findAll({ where:{ game_round_id, id:game_player_id }})
-
-    let wx_config = {}
-    let wx_share = {}
-    try {
-      // 分享链接应使用 checkin-wx，以便检查是否创建用户
-      // link = http://client.vw-dealer-wechat.faw-vw.com/wechatclient/game/#{game_round_id}/cupcheck_in/gotoGame.html?appid=#{appid}&code=null&state=null
-      let shareurl = `${GAME_HOST}/game-${game_round.code}/${game_round.id}/checkin-wx?to_game_player_id=${to_game_player.id}`
-      let apiurl = `${GET_WX_PARAM_URL}?authorizerAppid=${game_round.appid}&url=${encodeURIComponent(url)}&shareurl=${encodeURIComponent(shareurl)}`
-      console.log("playWx url=", apiurl)
-      let res = await fetch(apiurl, {
-        timeout: 2000,
-        method: 'post'
-      })
-      if (res) {
-        let data = await res.json()
-        console.log("playWx data=", data)
-        logger.info("playWx data=", data)
-        wx_config = {
-          appId: data['appId'],
-          timestamp: data['timestamp'],
-          nonceStr: data['nonceStr'],
-          signature: data['signature']
-        }
-        //const link = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx617b20b7ded64c67&redirect_uri=http%3A%2F%2Ftestwx.getstore.cn%2Fwapi%2Fv1%2Fwechatauth%2Fgameshareurl-done%3Fgameurl%3Dhttp%3A%2F%2Ftestwx.getstore.cn%2Fgame-bargain%2F${game_round.id}%2Fcheckin-wx%3Fto_game_player_id%3D${to_game_player.id}&response_type=code&scope=snsapi_userinfo&state=state#wechat_redirect`
-        //const link = `${GAME_HOST}/game-bargain/${game_round_id}/checkin-wx?to_game_player_id=${to_game_player.id}`
-        wx_share = {
-          link: data['link'],
-          img_url: `${GAME_HOST}/game-bargain-assets/app/images/share.jpg`
-=======
     // 点击投票，助力，砍价
     static async pollWx( ctx ){
 
@@ -366,7 +210,6 @@ console.log( "before to_game_player_id")
             }
         } else {
           data.error = { msg:  '底价和现价相等' }
->>>>>>> c31f2055345e8e77236cafbb1e884c2f395c108d:server/controllers/api/game/bargain.js
         }
       }
     } catch (err) {
