ba16d1351053fb226061abe756e5ba200e19c9b9
zhengxiuyu-at-getstore <zhengxiuyu@getstore.cn>
2019-04-19 08:20:08 +0800

    change something

--- server/models/index.js
+++ server/models/index.js
@@ -3,10 +3,35 @@ import GameRoundBargain from './GameRoundBargain'
 import GamePlayer from './GamePlayer'
 import GameResult from './GameResult'
 import GameDay from './GameDay'
+import IDoGameRound from './IDoGameRound'
+import IDoGameRoundStoreGift from './IDoGameRoundStoreGift'
+import IDoGift from './IDoGift'
+import IDoPlayer from './IDoPlayer'
+import IDoPlayerInfo from './IDoPlayerInfo'
+import IDoResult from './IDoResult'
+import IDoStroe from './IDoStroe'
 
-import { Sequelize } from '../schema'
+import {
+  Sequelize
+} from '../schema'
 
 
-GameResult.belongsTo( GamePlayer, {foreignKey: 'game_player_id'})
+GameResult.belongsTo(GamePlayer, {
+  foreignKey: 'game_player_id'
+})
 
-export  { Sequelize, GameRound, GameRoundBargain, GamePlayer,GameResult, GameDay }
+export {
+  Sequelize,
+  GameRound,
+  GameRoundBargain,
+  GamePlayer,
+  GameResult,
+  GameDay,
+  IDoGameRound,
+  IDoGameRoundStoreGift,
+  IDoGift,
+  IDoPlayer,
+  IDoPlayerInfo,
+  IDoResult,
+  IDoStroe
+}
