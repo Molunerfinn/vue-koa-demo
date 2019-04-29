8fd5a1b7bbb2d553bd68eaac1a6dab3799403ab4
zhengxiuyu-at-getstore <zhengxiuyu@getstore.cn>
2019-04-29 11:27:25 +0800

    update env

--- src/config/env.js
+++ src/config/env.js
@@ -6,16 +6,17 @@
  * baseImgPath: 图片存放地址
  *
  */
-let host =  process.env.GAME_HOST || 'localhost'
+ let host =  process.env.GAME_HOST || 'localhost'
+ let port =  process.env.GAME_HOST_PORT || '8080'
 let baseUrl = ''
 let routerMode = 'history'
 let baseImgPath = ''
 let baseImageUrl = ''
 if (process.env.NODE_ENV == 'development') {
   console.log( "host=",host, 'process.env.GAME_HOST', process.env.GAME_HOST, 'process.env.NODE_ENV', process.env.NODE_ENV)
-  baseUrl = `http://${host}:8080`
+  baseUrl = `http://${host}:${port}`
 } else {
-  baseUrl = `http://${host}:8080`
+  baseUrl = `http://${host}:${port}`
 }
 console.log( "baseUrl=",baseUrl)
 
