52189fadd12d484c9fc2ec65b27585f67d582eb6
qinghe <david@getstore.cn>
2019-04-29 12:15:45 +0800

    remove folder models/gamex

--- server/models/gamex/ido/GameRoundStoreGift.js
+++
@@ -1,4 +0,0 @@
-import { Sequelize, ido_gameround_store_gifts  } from '../../../schema'
-const IdoGameRoundStoreGift = ido_gameround_store_gifts
-
-export default IdoGameRoundStoreGift
--- server/models/gamex/ido/Result.js
+++
@@ -1,3 +0,0 @@
-import { Sequelize, ido_results  } from '../../../schema'
-const IdoResult = ido_results
-export default IdoResult
