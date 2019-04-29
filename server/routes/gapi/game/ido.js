b44644e53ffa9ed42431df4a38977bbed04128dd
zhengxiuyu-at-getstore <zhengxiuyu@getstore.cn>
2019-04-29 09:26:38 +0800

    add image

--- server/routes/gapi/game/ido.js
+++ server/routes/gapi/game/ido.js
@@ -4,6 +4,8 @@ const router = koaRouter()
 
 router.get('/start', ido.get_start_info)
 router.get('/result', ido.get_result_info)
+router.get('/login', ido.login)
+router.get('/get_wx_info', ido.get_wx_info)
 
 router.post('/sign_up', ido.post_sign_up)
 router.post('/thumb_up', ido.post_thumb_up)
