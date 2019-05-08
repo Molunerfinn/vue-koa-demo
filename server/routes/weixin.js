25f458c5b01c419708581b056fbcad0af7f2843c
qinghe <david@getstore.cn>
2019-05-08 09:43:58 +0800

    add weixin api sample

---
+++ server/routes/weixin.js
@@ -0,0 +1,31 @@
+/**
+ *
+ * @param {*} ctx
+ *            ctx.query.url
+ *            ctx.query.shareurl
+ */
+async getWxJsConfig(ctx) {
+    try {
+      let url = ctx.query.url
+      let shareurl = ctx.query.shareurl
+      var param = {
+       debug: false,
+       jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage'],
+       url: url
+      };
+      let ticket = await wechatApi.getLatestTicket()
+      console.debug( "getLatestTicket=", ticket, "url=", url )
+
+      let data  =  await wechatApi.getJsConfig(param);
+      if( shareurl ){
+        //var link = wechatOAuth.getAuthorizeURL(wechat_config.authdomain + '/wapi/v1/wechatauth/gameshareurl-done?shareurl='+shareurl, 'state', 'snsapi_userinfo');
+        var link = wechat_config.authdomain +'/wapi/v1/wechatauth/gameshareurl?shareurl='+encodeURIComponent(shareurl)
+        data.link = link
+      }
+      console.debug( " getWxJsConfig data = ", data )
+      ctx.body = data
+      ctx.status = 200
+    } catch (error) {
+        ctx.throw(messageContent.ResponeStatus.CommonError, 'can not get wx js config fail' + ': ' + error, { expose: true })
+    }
+},
