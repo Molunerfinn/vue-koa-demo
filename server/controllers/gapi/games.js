f33b26214192cd31115448b0302f8c7efc9dbf7c
zhengxiuyu-at-getstore <zhengxiuyu@getstore.cn>
2019-05-14 08:40:50 +0800

    fix bug

--- server/controllers/gapi/games.js
+++ server/controllers/gapi/games.js
@@ -1,108 +1,96 @@
 const messageContent = require('../constant')
 const {
-   getGameRoundModelByCode,
-   getGamePlayerModelByCode,
-   getGameResultModelByCode
+  getGameRoundModelByCode,
+  getGamePlayerModelByCode,
+  getGameResultModelByCode
 } = require('../../helpers/model')
-const { getWxJsConfig } = require('../../helpers/weixin')
+const {
+  getWxJsConfig
+} = require('../../helpers/weixin')
 
-// const WechatAPI = require('co-wechat-api');
-// var OAuth = require('co-wechat-oauth');
-// const config = require(`../../../config/wechat.${process.env.NODE_ENV}.json`);
-// const wechat_config = config.wechat;
-// const wechatApi = new WechatAPI(wechat_config.appid, wechat_config.secret);
-// var wechatOAuth = new OAuth(wechat_config.appid, wechat_config.secret);
+export default class GamesController {
 
-// url /:code/:number
-export default class GamesController{
+  // /**
+  //  * update round information, such as name, description
+  //  * @param {*} ctx
+  //  */
+  // async updateRound(ctx) {
+  //     var gameroundid = parseInt(ctx.params.id)
+  //     var game_round = ctx.request.body.game_round
+  //     try {
+  //         // tmp <Array.<affectedCount, affectedRows>>
+  //         var tmp = await dbOperation.MySqlOperation.UpdateRound(gameroundid, game_round)
+  //         console.log("dbOperation.MySqlOperation.UpdateRound1=", tmp )
+  //         if(tmp.length === 1){
+  //           ctx.status = 200
+  //         }
+  //     } catch (error) {
+  //         ctx.throw(messageContent.ResponeStatus.CommonError, `update round ${gameroundid} fail: ` + error, { expose: true })
+  //     }
+  // },
+  /**
+   * 取得游戏相关信息，并返回客户端，初始化游戏
+   * @param {*}
+   * @return {*}
+   */
+  static async getInfo(ctx) {
+    //try {
+    let code = ctx.params.code
+    let number = ctx.params.number
+    let parsed = ctx.request.body.parsed || {}
+    let openid = parsed.openid
 
-    // /**
-    //  * update round information, such as name, description
-    //  * @param {*} ctx
-    //  */
-    // async updateRound(ctx) {
-    //     var gameroundid = parseInt(ctx.params.id)
-    //     var game_round = ctx.request.body.game_round
-    //     try {
-    //         // tmp <Array.<affectedCount, affectedRows>>
-    //         var tmp = await dbOperation.MySqlOperation.UpdateRound(gameroundid, game_round)
-    //         console.log("dbOperation.MySqlOperation.UpdateRound1=", tmp )
-    //         if(tmp.length === 1){
-    //           ctx.status = 200
-    //         }
-    //     } catch (error) {
-    //         ctx.throw(messageContent.ResponeStatus.CommonError, `update round ${gameroundid} fail: ` + error, { expose: true })
-    //     }
-    // },
-    /**
-     * 取得游戏相关信息，并返回客户端，初始化游戏
-     * @param {*}
-     * @return {*}
-     */
-    static async getInfo(ctx) {
-        //try {
-            let code = ctx.params.code
-            let number = ctx.params.number
-            let parsed = ctx.request.body.parsed || {}
-            let openid = parsed.openid
+    let GameRound = getGameRoundModelByCode(code)
+    let GamePlayer = getGamePlayerModelByCode(code)
+    let GameResult = getGameResultModelByCode(code)
 
-            let GameRound = getGameRoundModelByCode(code)
-            let GamePlayer = getGamePlayerModelByCode(code)
-            let GameResult = getGameResultModelByCode(code)
-
-            // 取得游戏信息
-            let gameRound = await GameRound.findOne({
-              where: {
-                number
-              }
-            })
-            // 取得玩家信息
-            let gamePlayer = await GamePlayer.findOne({
-              where: {
-                game_round_id: gameRound.id,
-                openid: openid,
-              }
-            })
-            // 如果 gamePlayer 为 null， 检查是否需要创建
-            if( gamePlayer == null){
-              gamePlayer = {
-                openid: parsed.openid,
-                nickname: parsed.nickname,
-                avatar: parsed.headimgurl,
-                game_round_id: gameRound.id,
-                score: 0,
-                max_score: 0
-              }
-              if (gameRound.contact_required == 0) {
-                let res = await GamePlayer.create(gamePlayer)
-              }
-            }
-            let playerInfo = gamePlayer
-            if( gamePlayer.id ){
-              // 取得玩家相关信息
-              playerInfo = await gamePlayer.getInfo( )
-            }
-            // 每个游戏 GameRound
-            let gameInfo = await gameRound.getInfo( )
-            let wxConfig = getWxJsConfig()
-            var allInfo = {
-              gameRound: gameInfo,
-              gamePlayer: playerInfo,
-              wxConfig: wxConfig
-            }
-            ctx.body = allInfo
-
-        //} catch (error) {
-        //    ctx.throw(messageContent.ResponeStatus.CommonError, `show round ${ctx.params.id} fail: ` + error, { expose: true })
-        //}
+    // 取得游戏信息
+    let gameRound = await GameRound.findOne({
+      where: {
+        number
+      }
+    })
+    // 取得玩家信息
+    let gamePlayer = await GamePlayer.findOne({
+      where: {
+        game_round_id: gameRound.id,
+        openid: openid,
+      }
+    })
+    // 如果 gamePlayer 为 null， 检查是否需要创建
+    if (gamePlayer == null) {
+      gamePlayer = {
+        openid: parsed.openid,
+        nickname: parsed.nickname,
+        avatar: parsed.headimgurl,
+        game_round_id: gameRound.id,
+        score: 0,
+        max_score: 0
+      }
+      if (gameRound.contact_required == 0) {
+        let res = await GamePlayer.create(gamePlayer)
+      }
     }
+    let playerInfo = gamePlayer
+    if (gamePlayer.id) {
+      // 取得玩家相关信息
+      playerInfo = await gamePlayer.getInfo()
+    }
+    // 每个游戏 GameRound
+    let gameInfo = await gameRound.getInfo()
+    let wxConfig = getWxJsConfig()
+    var allInfo = {
+      gameRound: gameInfo,
+      gamePlayer: playerInfo,
+      wxConfig: wxConfig
+    }
+    ctx.body = allInfo
+
+    //} catch (error) {
+    //    ctx.throw(messageContent.ResponeStatus.CommonError, `show round ${ctx.params.id} fail: ` + error, { expose: true })
+    //}
   }
 
-  /**
-   * show game round
-   * @param {*} req
-   * @param {*} res
-   */
   static async addPlayer(ctx) {
     try {
       let code = ctx.params.code
@@ -135,11 +123,6 @@ export default class GamesController{
     }
   }
 
-  /**
-   * show game round
-   * @param {*} req
-   * @param {*} res
-   */
   static async setAchieve(ctx) {
     try {
       let code = ctx.params.code
@@ -153,9 +136,6 @@ export default class GamesController{
       })
     }
   }
-
-
-<<<<<<< HEAD
   /**
    * show game round
    * @param {*} req
@@ -167,28 +147,13 @@ export default class GamesController{
       let number = ctx.params.number
       console.log("showRoundByNumber= ", ctx.params)
       let Model = getGameRoundModelByCode(code)
-
+      ctx.body = {
+        success: true
+      }
     } catch (error) {
       ctx.throw(messageContent.ResponeStatus.CommonError, `show round ${ctx.params.id} fail: ` + error, {
         expose: true
       })
-=======
-    /**
-     * show game round
-     * @param {*} req
-     * @param {*} res
-     */
-    static async getRanking(ctx) {
-        try {
-            let code = ctx.params.code
-            let number = ctx.params.number
-            console.log("showRoundByNumber= ", ctx.params)
-            let Model = getGameRoundModelByCode(code)
-            ctx.body = { success: true }
-        } catch (error) {
-            ctx.throw(messageContent.ResponeStatus.CommonError, `show round ${ctx.params.id} fail: ` + error, { expose: true })
-        }
->>>>>>> abb0fa9ee4ca228ef8fd031c8f1b96fa9fd88854
     }
   }
 }
