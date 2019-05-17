72b0e641bd9ab69bd718934c49db319705aaa0a8
qinghe <david@getstore.cn>
2019-05-17 11:10:43 +0800

    Create game_constant.js

---
+++ src/game_constant.js
@@ -0,0 +1,33 @@
+// 当前游戏在
+
+module.exports = {
+  // created	   0 created	新建
+  //		open	     1 open	开始签到
+  //		ready	     2 ready	结束签到，准备开始
+  //		starting	 3 starting	开始前倒计时中
+  //		started	   4 started	游戏已开始
+  //		completed	 5 completed	游戏已结束
+  //		disabled	 6 disabled	游戏已关闭
+
+  DpGameRoundStates: {
+    created: 'created',
+    open: 'open',
+    ready: 'ready',
+    starting: 'starting',
+    started: 'started',
+    completed: 'completed',
+    disabled: 'disabled'
+  },
+  GameRoundStates: {
+    created: 'created',
+    open: 'open',
+    ready: 'ready',
+    starting: 'starting',
+    started: 'started',
+    completed: 'completed',
+    disabled: 'disabled'
+  },
+  GameConstant: {
+    maxTime: 99999.99 // 大屏游戏可以玩的最长时间，所有成绩需要小于这个时间
+  }
+}
