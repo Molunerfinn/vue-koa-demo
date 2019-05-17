64d2a4f7d4af5486b1ce50ff6a428bf23b70bbba
zhengxiuyu-at-getstore <zhengxiuyu@getstore.cn>
2019-05-17 11:37:05 +0800

    delete time

--- src/dpgames/pintu/RuleBox.vue
+++ src/dpgames/pintu/RuleBox.vue
@@ -59,11 +59,6 @@
              </div>
              <div id="selfAwardLine" class="hide" style="word-wrap: break-word;"></div>
            </div>
-
-           <div id="gameDateTime" class="poupLine" data-sortKey="c">
-             <div class="mainTitle">活动时间</div>
-             <p><span id="startDate">{{gameRound.start_at}}</span>&nbsp;-&nbsp;<span id="endDate">{{gameRound.end_at}}</span></p>
-           </div>
            <div class="hostLine poupLine" data-sortKey="d">
              <div class="mainTitle">主办单位</div>
              <p><a class="hostName" />{{gameRound.host}}</a>
