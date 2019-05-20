53f97eafc734dbec9ee61e0a442aaae318142eeb
qinghe <david@getstore.cn>
2019-05-20 09:54:55 +0800

    调整css

--- src/games/zhaobaba/main.js
+++ src/games/zhaobaba/main.js
@@ -5,8 +5,8 @@ import App from './App'
 // import ElementUI from 'element-ui'
 // import 'element-ui/lib/theme-default/index.css'
 import 'weui/dist/style/weui.css'
-import '@/assets/kouhong/css/new_base.css'
-import '@/assets/kouhong/css/new_game.css'
+import '@/assets/common/css/base2.css'
+import '@/assets/game/zhaobaba/css/game.css'
 
 //import EventBus  from '@/plugins/EventBus'
 //Vue.use(EventBus)
