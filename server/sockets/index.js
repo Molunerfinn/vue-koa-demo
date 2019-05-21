c47ffd959f4b81113263a13badfd0ec5993c429c
qinghe <david@getstore.cn>
2019-05-21 17:32:26 +0800

    add adapter redis

--- server/sockets/index.js
+++ server/sockets/index.js
@@ -1,11 +1,11 @@
 
 import IO from 'socket.io'
-//const redisAdapter  = require('socket.io-redis');
+const redisAdapter  = require('socket.io-redis');
 import DpPintuSocket from'./dpgame/pintu'
 
 export function sockets( server ){
   // {adapter: redisAdapter({ host: '127.0.0.1', port: 6379 })}
-  const io = IO(server, { transports: [ 'websocket' ] })
+  const io = IO(server, { transports: [ 'websocket' ], adapter: redisAdapter({ host: '127.0.0.1', port: 6379 }) })
   // 拼图socket
   DpPintuSocket.bind(io)
   return io
