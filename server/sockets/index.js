484303dfa5e504c270a65b39091173533009e13a
qinghe <david@getstore.cn>
2019-05-23 21:38:14 +0800

    删除log

--- server/sockets/index.js
+++ server/sockets/index.js
@@ -7,7 +7,7 @@ export function sockets( server ){
   let path = process.env.SOCKETIO_PATH || '/socket.io'
   const adapter =  redisAdapter({ host: '127.0.0.1', port: 6379 })
   const io = IO(server, { transports: [ 'websocket' ],  path,  adapter  })
-  console.log( "socket server bind path", path, io )
+  console.log( "socket server bind path", path )
   // 拼图socket
   DpPintuSocket.bind(io)
   return io
