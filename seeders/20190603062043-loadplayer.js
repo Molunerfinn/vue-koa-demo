86eecfaa74ca4c0e3e07e0fce00f667c6105c19c
qinghe <david@getstore.cn>
2019-06-04 16:58:03 +0800

    修正测试数据seed

--- seeders/20190603062043-loadplayer.js
+++ seeders/20190603062043-loadplayer.js
@@ -21,7 +21,7 @@ module.exports = {
     let avatar = 'http://thirdwx.qlogo.cn/mmopen/vi_32/vibBfZPicTeQwibjFyibXAWMmIl6UFvFhUR7xNibvsHic3sXM85q6PPF6FibxibJmkXHEKHd4UTDSfWic33a4FicnArPjYNw/132'
 
     let playerAttrs = [{openid: 'oF9hV0SyZ6tI_k2WHtpRXqfedRH4', nickname: 'nickname', avatar: avatar }]
-    for( let i=0; i<10000; i++ ){
+    for( let i=0; i<5000; i++ ){
       playerAttrs.push( { openid: `openid-${i}`, nickname:`nickname-${i}`, game_round_id: 7, avatar } )
     }
     logger.info( "20190603062043-loadplayer"  )
