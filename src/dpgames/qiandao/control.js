c3504e6c53904a28974129f462e8f68aa5544927
qinghe <david@getstore.cn>
2019-06-18 14:18:13 +0800

    rename control-base to control_base

--- src/dpgames/qiandao/control.js
+++ src/dpgames/qiandao/control.js
@@ -6,7 +6,7 @@ import Control from './Control.vue'
 // import 'element-ui/lib/theme-default/index.css'
 // import VueRouter from 'vue-router'
 import 'bootstrap/dist/css/bootstrap.css'
-import '@/assets/common/css/control-base.css'
+import '@/assets/common/css/control_base.css'
 import '@/assets/dpgame/yiy/css/control.css'
 
 
