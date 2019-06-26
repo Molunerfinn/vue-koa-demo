010f7e476f52ccaf267689aa644e65d6bc3a7a45
qinghe <david@getstore.cn>
2019-06-26 09:14:42 +0800

    add skinres

---
+++ src/games/zxg/game/skinres.js
@@ -0,0 +1,31 @@
+const skinRes = {}
+
+
+skinRes.runlin = {
+
+  "editPropListDef": [{
+      "name": "xg1",
+      "size": {
+        "height": "2.75rem",
+        "width": "4.5rem"
+      }
+    },
+    {
+      "name": "xg2",
+      "size": {
+        "width": "2.75rem",
+        "height": "4.5rem"
+      }
+    },
+    {
+      "name": "xg3",
+      "size": {
+        "width": "8.5rem",
+        "height": "13.5rem"
+      },
+    }
+  ]
+}
+
+
+export default skinRes
