f0cfd756f862ac58c3728727edad07cabb0dd968
zhengxiuyu-at-getstore <zhengxiuyu@getstore.cn>
2019-08-01 09:39:32 +0800

    add post and term api

--- server/routes/api/backend.js
+++ server/routes/api/backend.js
@@ -19,6 +19,10 @@ router.post('/base/modifyGameRound', base.modifyGameRound)
 router.post('/base/getWxMpUsers', base.getWxMpUsers)
 router.post('/base/modifyDesc', base.modifyDesc)
 
+router.post('/base/addPost', base.addPost)
+router.post('/base/addTerm', base.addTerm)
+router.post('/base/getTermInfo', base.getTermInfo)
+
 // router.post('/weixin_open', weixin.createRound)
 // router.post('/gameRound', gameRound.createRound)
 
